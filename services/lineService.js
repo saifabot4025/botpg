5df4582c56fe0bd10a8ab133525ae20c69404e3a
saifabot4025 <jingjai.coth@gmail.com>
2025-08-02 03:25:26 +0700

    Add files via upload

--- services/lineService.js
+++ services/lineService.js
@@ -1,30 +1,18 @@
+
 import line from "@line/bot-sdk";
 import dotenv from "dotenv";
+import { flexMenu } from "../utils/flexMenu.js";
 
 dotenv.config();
 
 const client = new line.Client({
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-export const replyMessage = async (replyToken, message) => {
-  try {
-    await client.replyMessage(replyToken, {
-      type: "text",
-      text: message,
-    });
-  } catch (err) {
-    console.error("❌ replyMessage Error:", err);
-  }
+export const replyMessage = async (replyToken, text) => {
+  await client.replyMessage(replyToken, { type: "text", text });
 };
 
 export const pushFlexMenu = async (userId) => {
-  try {
-    await client.pushMessage(userId, {
-      type: "text",
-      text: "นี่คือข้อความต้อนรับจาก LINE Bot 🎉",
-    });
-  } catch (err) {
-    console.error("❌ pushFlexMenu Error:", err);
-  }
+  await client.pushMessage(userId, flexMenu);
 };
--- services/telegramService.js
+++ services/telegramService.js
@@ -1,10 +1,11 @@
+
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
 
-export const sendTelegramAlert = async (text) => {
-  await bot.sendMessage(process.env.TELEGRAM_CHAT_ID, text);
+export const sendTelegramAlert = async (message) => {
+  await bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message, { parse_mode: "HTML", disable_web_page_preview: false });
 };
