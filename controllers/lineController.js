23a29734f896c4966e156f4c00b1c8b3c47d5074
saifabot4025 <jingjai.coth@gmail.com>
2025-08-02 02:55:23 +0700

    Update lineController.js

--- controllers/lineController.js
+++ controllers/lineController.js
@@ -1,11 +1,14 @@
 import { replyMessage, pushFlexMenu } from "../services/lineService.js";
-import { sendTelegramAlert } from "../services/telegramService.js";
 import { getGreetingMessage } from "../utils/messages.js";
 
 export const handleLineEvent = async (event) => {
-  if (event.type === "follow" || event.type === "message") {
-    const greeting = getGreetingMessage();
-    await replyMessage(event.replyToken, greeting);
-    await pushFlexMenu(event.source.userId);
+  try {
+    if (event.type === "follow" || event.type === "message") {
+      const greeting = getGreetingMessage();
+      await replyMessage(event.replyToken, greeting);
+      await pushFlexMenu(event.source.userId);
+    }
+  } catch (err) {
+    console.error("❌ handleLineEvent Error:", err);
   }
 };
